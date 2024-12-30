aa8e3e712446308deb89196d724696da1d62f37b
kriszta99 <lukacs.krisztina@student.ms.sapientia.ro>
2024-12-30 18:48:15 +0200

    movies api connected

--- api/index.py
+++ api/index.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
+# Flask alkalmazás inicializálása
 app = Flask(__name__)
+# TMDB API kulcs
+API_KEY = 'b0ff54d9f03d74a916ff2ecc5fa2ccd0'  # Cseréld le a saját API kulcsodra
+BASE_URL = 'https://api.themoviedb.org/3/'
 
+# Funkció a legnépszerűbb filmek lekérésére a TMDB API-ról
+def get_popular_movies():
+    url = f"{BASE_URL}movie/popular?api_key={API_KEY}&language=en-US&page=1"
+    response = requests.get(url)
+    return response.json()['results']
+
+# Főoldal route, ahol megjelenítjük a népszerű filmeket
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    # Filmek lekérése
+    movies = get_popular_movies()
+    return render_template('index.html', movies=movies)
 
-@app.route('/about')
-def about():
-    return 'About'
+# Alkalmazás indítása
+if __name__ == '__main__':
+    app.run(debug=True)
