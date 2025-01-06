ae14f874375e27ef7b6eeeb780869830896445da
kriszta99 <lukacs.krisztina@student.ms.sapientia.ro>
2025-01-06 11:26:39 +0200

    update

--- api/index.py
+++ api/index.py
@@ -43,90 +43,109 @@ def get_popular_movies():
 movies = get_popular_movies()
 
 def remove_duplicates(movies):
-    # az 'id' kulcskent hasznalva 
+    # letrehozunk egy ures szotarat s a kulcsok a filmek 'id' lesz
     movie_dict = {}
     for movie in movies:
-        movie_id = movie['id']  # Minden film azonosítójának lekérése
-        movie_dict[movie_id] = movie  # A szótárban az azonosítóval tároljuk a filmet
+        movie_id = movie['id']  # minden film id-ja lekerese
+        # ha a film id-ja letezik a szotarban akkor az uj film felirodik a regivel s csak 1 film marad benne
+        movie_dict[movie_id] = movie  
     
-    # A szótár értékeit alakítjuk listává
+    # a szotar ertekeit atalakitjuk listava
     unique_movies = list(movie_dict.values())
     return unique_movies
 
 def filter_movies_by_user_pref(user_pref):
-    return [movie for movie in movies if any(genre_id in user_pref for genre_id in movie['genre_ids'])]
+    result = []
+    for movie in movies:
+        for genre_id in movie['genre_ids']:
+            if genre_id in user_pref:
+                result.append(movie)
+                break  # ha talaunk egyet kilepunk
+    return result
+
 
 def population_inicialization(user_pref):
     return random.sample(movies, 3) 
 
 
 def fitness(individual, user_pref):
-    # Filmek pontszámainak tárolása
+    # a filmek pontszamainak tarolasa
     score_per_movie = []
     for movie in individual:
-        genre_ids = movie.get("genre_ids", [])  # A film műfajainak ID-jait szedjük ki
+        genre_ids = movie.get("genre_ids", [])  # a film mufajainak ID-jait szedjuk ki
         
-        # A felhasználó preferált műfajainak ID-jai
         match_score = 0
+        #a film mufajainak (genre_ids) azonositoit osszehasonlitjuk a felhasznaloi preferenciakkal (user_pref)
         for genre_id in genre_ids:
             if genre_id in user_pref:
-                match_score += 1  # Egyezésnél növeljük a pontszámot
+                match_score += 1  # egyezésnél növeljük a pontszámot
         
-        # Minden filmhez hozzáadjuk a pontszámot
+        # minden filmhez hozzaadjuk a pontszamot
         score_per_movie.append(match_score)
 
-    # Átlagpontszám kiszámítása az egyedhez (filmszett)
-    average_score = sum(score_per_movie) / len(score_per_movie) if score_per_movie else 0
+    # atlaga kiszamitasa az egyedhez (filmszett)
+    if score_per_movie:
+        average_score = sum(score_per_movie) / len(score_per_movie)
+    else:
+        average_score = 0
 
-    # Az összes műfaj ID összevetése a felhasználó preferenciáival
-    all_genres_in_individual = [genre_id for movie in individual for genre_id in movie.get("genre_ids", [])]
-    # Diverzitás bónusz: különböző műfajok egyezése, ami megnöveli a relevanciát
+    # a filmszetten vegigmegyunk s kinyerjuk az osszes film mufaj id-jat s hozzaadjuk ezeket 
+    all_genres_in_individual = []
+    for movie in individual:
+        all_genres_in_individual.extend(movie.get("genre_ids", []))
+    # diverzitas bonusz: kulonbozo mufajok egyezese, ami megnoveli a relevanciat
+    # a filmszett es a felasznalo preferenciak kozzul a kozoseket kiveszik -> hossza lesz a diverzitas bonusz
     diversity_bonus = len(set(user_pref) & set(all_genres_in_individual))
 
-    # Súlyozott bónusz: az átlagpontszámhoz hozzáadott diverzitás
-    final_score = average_score + (diversity_bonus * 0.5)  # A bónusz súlyozása finomítva
+    # a sulyozott bonusz: az atlaga es a hozzáadott diverzitas
+    final_score = average_score + (diversity_bonus * 0.5)
 
     return final_score
 
-
 def crossover(parent1, parent2, user_pref):
-    # Véletlenszerű keresztezés
+    # veletlenszeru keresztezes
     split = random.randint(1, min(len(parent1), len(parent2)) - 1)
 
-    # A szülők filmlistáját kombináljuk
+    # utod letrehozasa ket szulo kombinalasaval
     child = parent1[:split] + parent2[split:]
     
     return child
 
-
+# mutacio
 def mutate(individual, user_pref):
     mutation_point = random.randint(0, len(individual) - 1)
 
-    # A mutáció során egy véletlen filmet választunk, amely illeszkedik a műfaj preferenciákhoz
+    # a mutacio soran egy veletlen filmet valasztunk, amely illeszkedik a mufaj preferenciakhoz
     valid_movies = filter_movies_by_user_pref(user_pref)
 
-    if valid_movies:  # Ha van érvényes film
+    if valid_movies:  # Ha van ervenyes film
         individual[mutation_point] = random.choice(valid_movies)
 
     return individual
 
 
 def genetic_algorithm(population_size, generations, user_pref):
-    population = [population_inicialization(user_pref) for _ in range(population_size)]
+    # az osszes filmszetett tartalmazza
+    population = []
+    for _ in range(population_size):
+        population.append(population_inicialization(user_pref))
  
    
     for generation in range(generations):
+        #itt  tortenik a kivalasztas
+        # csokkeno sorrendbe rendezi hogy elore keruljenek a legjobb egyed (a legmagasabb fitness pontszamuak)
         population.sort(key=lambda x: fitness(x, user_pref), reverse=True)
-
+        # az elso ketot veszi ki
         parents = population[:2]
+
         new_population = []
 
 
         # Keresztezes es mutacio minden uj egyedhez
         for _ in range(population_size // 2):
-            #keresztezes
+            #keresztezes -> ket szulo kombinalasaval egy uj gyerek jon letre
             child = crossover(parents[0], parents[1], user_pref)
-            #mutacio
+            #mutacio -> a gyerek egyedet modositja egy veletlenszeru mutacioval
             child = mutate(child, user_pref)
             #uj egyed hozzaadasa
             new_population.append(child)
@@ -138,19 +157,29 @@ def genetic_algorithm(population_size, generations, user_pref):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    #lekerem a mufajokat a listaba
     genres = get_genres()  
     selected_genres = []  
     user_pref = []  
     best_movies = [] 
     error_message = None
 
+    #ha POST keres van 
     if request.method == 'POST':
-        selected_ids = request.form.getlist('genre')  # mufajok id-jait 
-        genre_dict = {str(genre['id']): genre['name'] for genre in genres}
-        selected_genres = [genre_dict[genre_id] for genre_id in selected_ids if genre_id in genre_dict]
+        selected_ids = request.form.getlist('genre')  # mufajok id-jait lekerjuk a formbol
+
+        genre_dict = {}
+        for genre in genres:
+            genre_dict[str(genre['id'])] = genre['name']
+
+        selected_genres = []
+        for genre_id in selected_ids:
+            if genre_id in genre_dict:
+                selected_genres.append(genre_dict[genre_id])
 
         user_pref = [int(genre_id) for genre_id in selected_ids]  
-        if not user_pref:
+        
+        if not selected_ids:
             error_message = "Please select at least one genre."
             return render_template('index.html', genres=genres, error_message=error_message)
 
