2542746f87fcb15316bb5c2e69f3165da1456dbb
kriszta99 <lukacs.krisztina@student.ms.sapientia.ro>
2025-02-28 13:34:25 +0200

    movie and tv shows recommendation system

--- api/index.py
+++ api/index.py
@@ -8,10 +8,27 @@ API_KEY = 'b0ff54d9f03d74a916ff2ecc5fa2ccd0'
 BASE_URL = 'https://api.themoviedb.org/3/'
 
 URL = "https://api.themoviedb.org/3/movie/popular"
+URL_tv = "https://api.themoviedb.org/3/tv/popular"
 PARAMS = {
     "api_key": API_KEY,
     "language": "en-US",
 }
+
+"""
+def remove_duplicates(movies):
+    # letrehozunk egy ures szotarat s a kulcsok a filmek 'id' lesz
+    movie_dict = {}
+    for movie in movies:
+        movie_id = movie['id']  # minden film id-ja lekerese
+        # ha a film id-ja letezik a szotarban akkor az uj film felirodik a regivel s csak 1 film marad benne
+        movie_dict[movie_id] = movie  
+    
+    # a szotar ertekeit atalakitjuk listava
+    unique_movies = list(movie_dict.values())
+    return unique_movies
+"""
+def remove_duplicates(movies):
+    return list({movie['id']: movie for movie in movies}.values())
 
 # lekertem a TMDB API ból a filmek mufajait/kategoriajit
 def get_genres():
@@ -25,10 +42,29 @@ def get_genres():
         print(f"Error fetching genres: {e}")
         return []
 
+
+#lekerem a legnepszerubb sorozatokat a TMDB-bol
+def get_popular_tv_shows():
+    all_tv_shows = []
+    for page in range(1,25): 
+        # mivel tudom hogy egy oldalon 20 darab film van igy oldalankent kerem le
+        PARAMS['page'] = page
+        response = requests.get(URL_tv, params=PARAMS)
+        if response.status_code == 200:
+            data = response.json()
+            popular_tv_shows = data.get("results", [])
+            all_tv_shows.extend(popular_tv_shows)
+        else:
+            print(f"Error fetching page {page}: {response.status_code}")
+    return all_tv_shows
+
+
+
+
 #lekerem a legnepszerubb filmeket TMDB API-ból
 def get_popular_movies():
     all_movies = []
-    for page in range(1, 51):  # oldalnyi filmet kerek le mivel 1 oldalon 20 darav vab
+    for page in range(1, 25):  # oldalnyi filmet kerek le mivel 1 oldalon 20 darab van
         PARAMS['page'] = page
         response = requests.get(URL, params=PARAMS)
         if response.status_code == 200:
@@ -38,19 +74,6 @@ def get_popular_movies():
         else:
             print(f"Error fetching page {page}: {response.status_code}")
     return all_movies
-
-
-def remove_duplicates(movies):
-    # letrehozunk egy ures szotarat s a kulcsok a filmek 'id' lesz
-    movie_dict = {}
-    for movie in movies:
-        movie_id = movie['id']  # minden film id-ja lekerese
-        # ha a film id-ja letezik a szotarban akkor az uj film felirodik a regivel s csak 1 film marad benne
-        movie_dict[movie_id] = movie  
-    
-    # a szotar ertekeit atalakitjuk listava
-    unique_movies = list(movie_dict.values())
-    return unique_movies
 
 
 
@@ -80,6 +103,7 @@ def fitness(individual, user_pref):
 def crossover(parent1, parent2):
     # veletlenszeru keresztezes
     #print(f"Paren1 :{parent1}")
+    #azert indul 1 tol hogy legyen legalabb egy elem mindket szulobol az utodban
     split = random.randint(1, len(parent1) - 1)
     
     # utod letrehozasa ket szulo kombinalasaval
@@ -90,12 +114,10 @@ def crossover(parent1, parent2):
 
 # mutacio
 def mutate(individual, movies):
+    #0-tol mivel barmelyik elem kivalaszthato
     mutation_point = random.randint(0, len(individual) - 1)
     individual[mutation_point] = random.choice(movies)
     return individual
-
-
- 
 
 def genetic_algorithm(population_size, generations, movies, user_pref):
     # az osszes filmszetett tartalmazza
@@ -113,20 +135,19 @@ def genetic_algorithm(population_size, generations, movies, user_pref):
         new_population = []
 
 
-        # Keresztezes es mutacio minden uj egyedhez
+        # uj generacio letrehozasa
         for _ in range(population_size // 2):
     
             #keresztezes -> ket szulo kombinalasaval egy uj gyerek jon letre
             child = crossover(parents[0], parents[1])
             #print(parents[0], parents[1])
-            #mutacio -> a gyerek egyedet modositja egy veletlenszeru mutacioval
-            
+            #mutacio -> a gyerek egyed elemet modositja egy veletlenszeru mutacioval     
             child = mutate(child, movies)
             #uj egyed hozzaadasa
             new_population.append(child)
             
             
-
+        
         population = new_population
 
     population.sort(key=lambda x: fitness(x, user_pref), reverse=True)
@@ -142,7 +163,9 @@ def index():
     selected_genres = []  
     user_pref = []  
     best_movies = [] 
+    best_tv_shows = []
     error_message = None
+    tv_shows = []
 
     #ha POST keres van 
     if request.method == 'POST':
@@ -166,8 +189,12 @@ def index():
         movies = get_popular_movies()
         best_movies = genetic_algorithm(population_size=100, generations=50,  movies=movies, user_pref=user_pref)
 
-        #print(f"Best movies: {best_movies}")
-    return render_template('index.html', genres=genres, selected_genres=selected_genres, best_movies=best_movies)
+        tv_shows = get_popular_tv_shows()
+        best_tv_shows = genetic_algorithm(population_size=100, generations=50,  movies=tv_shows, user_pref=user_pref)
+
+
+        print(f"Best tv shows: {best_tv_shows}")
+    return render_template('index.html', genres=genres, selected_genres=selected_genres, best_movies=best_movies, best_tv_shows=best_tv_shows)
 
 if __name__ == '__main__':
     app.run(debug=True)
